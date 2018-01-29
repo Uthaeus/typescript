dfde49c5ab908ba9f29e8804ec010be4af3c1467
Uthaeus <Uth@github.com>
2018-01-29 14:12:41 -0700

    first two lessons

---
+++ 006_variables.js
@@ -0,0 +1,7 @@
+var fullName = "Homer Simpson";
+var paidAccount = true;
+var versionNumber = 1.3;
+console.log(fullName);
+console.log(paidAccount);
+console.log(versionNumber);
+//# sourceMappingURL=006_variables.js.map
---
+++ 006_variables.ts
@@ -0,0 +1,8 @@
+var fullName : string = "Homer Simpson";
+let paidAccount : boolean = true;
+const versionNumber : number = 1.3;
+
+console.log(fullName);
+console.log(paidAccount);
+console.log(versionNumber);
+
