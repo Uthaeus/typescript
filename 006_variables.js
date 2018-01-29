bb3d394118a98f0f69dc83f81a0035fc0e38592b
Uthaeus <Uth@github.com>
2018-01-29 14:30:41 -0700

    updated second lesson

--- 006_variables.js
+++ 006_variables.js
@@ -1,7 +1,14 @@
-var fullName = "Homer Simpson";
-var paidAccount = true;
-var versionNumber = 1.3;
-console.log(fullName);
-console.log(paidAccount);
-console.log(versionNumber);
+// var fullName : string = "Homer Simpson";
+// let paidAccount : boolean = true;
+// const versionNumber : number = 1.3;
+// fullName = "Marge";
+// paidAccount = false;
+// console.log(fullName);
+// console.log(paidAccount);
+// console.log(versionNumber);
+function printName(f, l) {
+    var greeting = "Hi, there, ";
+    console.log(greeting + f + " " + l);
+}
+printName("Homer", "Simpson");
 //# sourceMappingURL=006_variables.js.map
--- 006_variables.ts
+++ 006_variables.ts
@@ -1,8 +1,17 @@
-var fullName : string = "Homer Simpson";
-let paidAccount : boolean = true;
-const versionNumber : number = 1.3;
+// var fullName : string = "Homer Simpson";
+// let paidAccount : boolean = true;
+// const versionNumber : number = 1.3;
 
-console.log(fullName);
-console.log(paidAccount);
-console.log(versionNumber);
+// fullName = "Marge";
+// paidAccount = false;
 
+// console.log(fullName);
+// console.log(paidAccount);
+// console.log(versionNumber);
+
+function printName(f, l) {
+  let greeting : string = "Hi, there, ";
+  console.log(greeting + f + " " + l);
+}
+
+printName("Homer", "Simpson");
