bc71fe899dcbde16d085bd8da64ee32dd8aeb79e
Uthaeus <Uth@github.com>
2018-01-29 15:27:52 -0700

    lesson type alias

---
+++ 008_type_alias.js
@@ -0,0 +1,3 @@
+var players = ["Altuve", "Correa", "Bregman"];
+console.log(players);
+//# sourceMappingURL=008_type_alias.js.map
---
+++ 008_type_alias.ts
@@ -0,0 +1,3 @@
+type PlayerArray = Array<string>;
+let players : PlayerArray = ["Altuve", "Correa", "Bregman"];
+console.log(players);
