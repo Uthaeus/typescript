d232d223e14c645f77af73fa2a554f5eb3fef145
Uthaeus <Uth@github.com>
2018-01-29 15:46:31 -0700

    lesson union types and arithmetic operators

---
+++ 009_union_types.ts
@@ -0,0 +1,11 @@
+type PlayerArray = Array<string|number>;
+let players : PlayerArray = ["Altuve", "Correa", "Bregman"];
+let player_numbers : PlayerArray = [25, 3, 2];
+console.log(players);
+console.log(player_numbers);
+
+var names : string[]|string;
+names = ["Homer Simpson", "Bart Simpson"];
+console.log(names);
+names = "Lisa Simpson";
+console.log(names);
