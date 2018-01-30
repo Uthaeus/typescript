e301976cd191981c709d2362f8088ea57588ef53
Uthaeus <Uth@github.com>
2018-01-30 16:31:50 -0700

    lesson closure introductions

---
+++ 019_closure_introductions.ts
@@ -0,0 +1,47 @@
+//Functions have access to any public variables in the outer scope
+
+// function nameFunction(name: string) : void {
+//   var n : string = name;
+
+//   function printName() {
+//     console.log(n);
+//   }
+//   printName();
+// }
+
+// nameFunction("Homer");
+//_____________________________
+
+// Inner functions maintain acess to the outer scope even AFTER the values are returned!
+
+
+// function nameFunction(name: string) {
+//   var n : string = name;
+
+//   return function() {
+//     console.log(n);
+//   }
+// }
+
+// var nameAgain = nameFunction('Homer');
+// nameAgain();
+//______________________________
+
+// 
+
+function lineup() {
+  var nowBatting : number = 1;
+
+  return {
+    nextBatter() { nowBatting++ },
+    currentBatter() { return nowBatting }
+  }
+}
+
+let batters = lineup();
+
+console.log(batters.currentBatter());
+batters.nextBatter();
+console.log(batters.currentBatter());
+batters.nextBatter();
+console.log(batters.currentBatter());
