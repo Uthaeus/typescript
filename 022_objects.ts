b0169298394965e0556cfd13dde46bd65a4ac5e5
Uthaeus <Uth@github.com>
2018-01-31 13:09:09 -0700

    lesson objects

---
+++ 022_objects.ts
@@ -0,0 +1,13 @@
+var realUser = {
+  email: 'test@test.com',
+  firstName: 'Homer',
+  lastName: 'Simpson',
+  sayHi() {
+    return "Hey there!";
+  }
+};
+
+console.log(realUser.email);
+console.log(realUser.firstName);
+console.log(realUser.lastName);
+console.log(realUser.sayHi());
